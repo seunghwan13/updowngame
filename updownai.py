c824ba2cca8317634d48e09cb98a61d3a7cc2542
seunghwan13 <childwelfare23@gmail.com>
2022-01-07 16:59:57 +0900

    나머지 내용 커밋

---
+++ updownai.py
@@ -0,0 +1,24 @@
+num = int(input("1~100 사이의 수를 입력하세요"))
+
+first = 25
+last = 100
+print(first)
+val = input("up, down, answer 중 알맞은 값 하나를 입력하시오.")
+
+while True:
+    for j in range(5):
+        median = int((first+last)/2)
+        print(median)
+        val = input("up, down, answer 중 알맞은 값 하나를 입력하시오.")
+
+        if(val == "up"):
+            first = median
+        elif(val == "down"):
+            last = median
+        elif(val == "answer"):
+            print("정답입니다")
+            print("게임이 끝났습니다")
+            quit()
+        else:
+            print("다시 입력해주세요.")
+            break
